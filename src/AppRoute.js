4abedb29599897e0016d9be8c93fc907cf9dd49c
Naihua Chen <naihua.chen@mail.goldennet.com.tw>
2022-01-07 11:02:31 +0800

    [Update -- Modify 實測 html2canvas 環境建立]

--- src/AppRoute.js
+++ src/AppRoute.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { HashRouter  as Router, Route } from "react-router-dom";
 
 import Login from "./Login"; //登入
-import Test from "./Directory"; //登入
+import Test from "./view/Directory";
+import Canvas from "./view/html2canvanPage"
 
 const AppRoute = (props) => {
     return (
         <Router>
             <Route exact path="/"><Login /></Route> {/* 登入 */}
-            <Route exact path="/test"><Test /></Route> {/* 登入 */}
+            <Route exact path="/test"><Test /></Route>
+            <Route exact path="/canvas"><Canvas /></Route>
         </Router>
     )
 }
