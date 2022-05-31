b65bfdc7c4eacf78c2387cae81d5a41087cc0f51
Naihua Chen <naihua.chen@mail.goldennet.com.tw>
2022-05-31 13:48:11 +0800

    [Update -- close button]

--- src/view/ClosePage.js
+++ src/view/ClosePage.js
@@ -10,9 +10,20 @@ const ClosePage = () => {
         // window.open('', '_self');
         // window.close();
     }
+    const handleClosePage2 = () =>{
+        let opened = window.open('about:blank','_self')
+        opened.opener = null
+        opened.close();
+        console.log('關閉')
+
+        // window.opener = null;
+        // window.open('', '_self');
+        // window.close();
+    }
     return (
         <>
             <button className='btn btn-primary' onClick={handleClosePage}>關閉</button>
+            <button className='btn btn-primary' onClick={handleClosePage2}>關閉2</button>
         </>
     )
 }
